936d7856a52fa3eaf2a6d83f77838d174fbc8587
jorianbeukens <71484@hoornbeeck.nl>
2025-04-23 21:42:54 +0200

    fix pluginupdater

--- plugin-updater.php
+++ plugin-updater.php
@@ -31,32 +31,45 @@ function jb_update_plugin_from_github() {
 
     function delete_directory($dir) {
         if (!is_dir($dir)) return;
-            $items = array_diff(scandir($dir), array('.', '..'));
-            foreach ($items as $item) {
-                $path = $dir . DIRECTORY_SEPARATOR . $item;
-                is_dir($path) ? delete_directory($path) : unlink($path);
-            }
-            rmdir($dir);
+        $items = array_diff(scandir($dir), array('.', '..'));
+        foreach ($items as $item) {
+            $path = $dir . DIRECTORY_SEPARATOR . $item;
+            is_dir($path) ? delete_directory($path) : unlink($path);
         }
-        $download_url = 'https://github.com/jorian2005/DienstverleningVDS-ProductPlugin/archive/refs/heads/main.zip';
-        $tmp_file = download_url($download_url);
-
-    $target_dir = $extract_to . '/dienstverleningvds-productplugin';
-    if (is_dir($target_dir)) {
-        delete_directory($target_dir);
+        rmdir($dir);
     }
-    rename($extract_to . '/DienstverleningVDS-ProductPlugin-main', $target_dir);
-    echo '<div class="notice notice-success"><p>Plugin succesvol bijgewerkt vanaf GitHub!</p></div>';
 
+    $download_url = 'https://github.com/jorian2005/DienstverleningVDS-ProductPlugin/archive/refs/heads/main.zip';
+    $tmp_file = download_url($download_url);
     $extract_to = WP_PLUGIN_DIR;
+
     $unzip_result = unzip_file($tmp_file, $extract_to);
 
     if (is_wp_error($unzip_result)) {
         echo '<div class="notice notice-error"><p>Fout bij uitpakken: ' . esc_html($unzip_result->get_error_message()) . '</p></div>';
     } else {
-        rename($extract_to . '/DienstverleningVDS-ProductPlugin-main', $extract_to . '/dienstverleningvds-productplugin');
+        $old_path = $extract_to . '/DienstverleningVDS-ProductPlugin-main';
+        $new_path = $extract_to . '/dienstverleningvds-productplugin';
+
+        if (is_dir($new_path)) {
+            delete_directory($new_path);
+        }
+
+        rename($old_path, $new_path);
+
         echo '<div class="notice notice-success"><p>Plugin succesvol bijgewerkt vanaf GitHub!</p></div>';
+
+        $plugin_main_file = 'dienstverleningvds-productplugin/dienstverleningvds-productplugin.php';
+
+        $result = activate_plugin($plugin_main_file);
+
+        if (is_wp_error($result)) {
+            echo '<div class="notice notice-error"><p>Fout bij activeren plugin: ' . esc_html($result->get_error_message()) . '</p></div>';
+        } else {
+            echo '<div class="notice notice-success"><p>Plugin is geactiveerd!</p></div>';
+        }
     }
 
     @unlink($tmp_file);
 }
+
